a15f41392c9349beb334b0717f395b5559ecdeb7
Alex Tearse-Doyle <alextearsedoyle@gmail.com>
2018-09-30 18:58:35 -0700

    suppress DefOf warning on load

--- Source/What_Is_My_Purpose.cs
+++ Source/What_Is_My_Purpose.cs
@@ -2,6 +2,7 @@
 using Verse;
 using UnityEngine;
 using Harmony;
+using RimWorld;
 
 namespace What_Is_My_Purpose
 {
@@ -15,7 +16,18 @@ namespace What_Is_My_Purpose
 			HarmonyInstance.DEBUG = true;
 #endif
 			HarmonyInstance harmony = HarmonyInstance.Create("Uuugggg.rimworld.What_Is_My_Purpose.main");
+			
+			//Turn off DefOf warning since harmony patches trigger it.
+			harmony.Patch(AccessTools.Method(typeof(DefOfHelper), "EnsureInitializedInCtor"),
+				new HarmonyMethod(typeof(Mod), "EnsureInitializedInCtorPrefix"), null);
+			
 			harmony.PatchAll();
+		}
+
+		public static bool EnsureInitializedInCtorPrefix()
+		{
+			//No need to display this warning.
+			return false;
 		}
 
 		public override void DoSettingsWindowContents(Rect inRect)
